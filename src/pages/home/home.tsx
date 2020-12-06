3c99c1457320f051cf59b8109a186ed527b1941d
kainsteffen <tranduykhanh1996@yahoo.de>
2020-12-06 14:50:35 +0100

    feat(home): add earnings dates track

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,6 +1,7 @@
 import { Box, Container, Grid, Typography } from "@material-ui/core";
 import Axios from "axios";
 import React, { useEffect } from "react";
+import EarningsDateCard from "../../components/earnings-date-card/earnings-date-card";
 import MarketNewsCard from "../../components/market-news-card/market-news-card";
 import TrendingStockCard from "../../components/trending-stock-card/trending-stock-card";
 
@@ -28,9 +29,46 @@ export default function Home() {
     <Container>
       <Box marginBottom={3}>
         <Typography variant="h6" gutterBottom>
+          Upcoming Events
+        </Typography>
+        <Grid container spacing={3} direction="row">
+          <Grid item xs={6} md={4} lg={3}>
+            <EarningsDateCard
+              imgSrc="//logo.clearbit.com/apple.com?size=100"
+              ticker="AAPL"
+              name="Apple Earnings Call"
+              date="Dec 16 2020"
+              expectedEPS={2.51}
+              currentEPS={3.2}
+            />
+          </Grid>
+          <Grid item xs={6} md={4} lg={3}>
+            <EarningsDateCard
+              imgSrc="//logo.clearbit.com/microsoft.com?size=100"
+              ticker="MSFT"
+              name="Microsoft Earnings Call"
+              date="Dec 05 2020"
+              expectedEPS={3.31}
+              currentEPS={4.31}
+            />
+          </Grid>
+          <Grid item xs={6} md={4} lg={3}>
+            <EarningsDateCard
+              imgSrc="//logo.clearbit.com/amazon.com?size=100"
+              ticker="AMZN"
+              name="Amazon General Assembly"
+              date="Dec 05 2020"
+              expectedEPS={1.11}
+              currentEPS={2.76}
+            />
+          </Grid>
+        </Grid>
+      </Box>
+
+      <Box marginBottom={3}>
+        <Typography variant="h6" gutterBottom>
           Trending
         </Typography>
-
         <Grid container spacing={3} direction="row">
           <Grid item xs={6} md={8} lg={2}>
             <TrendingStockCard />
