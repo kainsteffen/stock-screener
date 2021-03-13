3e3a46b02ec1827d1259e31016777431ec2410ca
kainsteffen <tranduykhanh1996@yahoo.de>
2021-03-13 16:40:47 +0100

    refactor(theme): decouple custom theme

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,8 @@ import {
   Badge,
   Box,
   Container,
-  createMuiTheme,
   createStyles,
   CssBaseline,
-  fade,
   IconButton,
   makeStyles,
   Theme,
@@ -26,73 +24,7 @@ import Home from "./pages/home/home";
 import Strategies from "./pages/strategies/strategies";
 import StrategyDetail from "./pages/strategy-detail/strategy-detail";
 import SymbolDetail from "./pages/symbol-detail/symbol-detail";
-
-declare module "@material-ui/core/styles/createMuiTheme" {
-  interface Theme {
-    customColors: {
-      numberChange: {
-        positive: {
-          color: React.CSSProperties["color"];
-          backgroundColor: React.CSSProperties["color"];
-        };
-        negative: {
-          color: React.CSSProperties["color"];
-          backgroundColor: React.CSSProperties["color"];
-        };
-      };
-    };
-  }
-  interface ThemeOptions {
-    customColors: {
-      numberChange: {
-        positive: {
-          color: React.CSSProperties["color"];
-          backgroundColor: React.CSSProperties["color"];
-        };
-        negative: {
-          color: React.CSSProperties["color"];
-          backgroundColor: React.CSSProperties["color"];
-        };
-      };
-    };
-  }
-}
-
-const theme = createMuiTheme({
-  overrides: {
-    MuiCssBaseline: {
-      "@global": {
-        ".MuiCard-root": {
-          borderRadius: "8px",
-        },
-        ".MuiButton-root": {
-          borderRadius: "500px",
-        },
-      },
-    },
-  },
-  palette: {
-    type: "dark",
-    primary: {
-      light: "#757ce8",
-      main: "#1EB980",
-      dark: "#045D56",
-      contrastText: "#fff",
-    },
-  },
-  customColors: {
-    numberChange: {
-      positive: {
-        color: "#41CE3E",
-        backgroundColor: fade("#41CE3E", 0.15),
-      },
-      negative: {
-        color: "#FF6262",
-        backgroundColor: fade("#FF6262", 0.15),
-      },
-    },
-  },
-});
+import { theme } from "./theme";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
